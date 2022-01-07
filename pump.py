0c5d0bc4e9a706501a39a14f0e9d4449143d0024
Hanne Hasbach <hanne.hasbach@student.hs-rm.de>
2022-01-07 14:13:36 +0100

    Pump datei

---
+++ pump.py
@@ -0,0 +1,7 @@
+#wird aufgerufen wenn Befehl gegeben wurde
+#Methode für jede Pumpe oder eine für alle? Anhand der Pins unterscheiden?
+#starte die Pumpe
+def startPump():
+        print("YEAAAH")
+
+
