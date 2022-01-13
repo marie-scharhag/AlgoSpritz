7a59517cbe845fcce27798b089be0ae26b7b957f
Hanne Hasbach <hanne.hasbach@student.hs-rm.de>
2022-01-13 17:38:13 +0100

    Pumpe läuft im 10sec Takt

--- pump.py
+++ pump.py
@@ -1,7 +1,13 @@
 #wird aufgerufen wenn Befehl gegeben wurde
 #Methode für jede Pumpe oder eine für alle? Anhand der Pins unterscheiden?
 #starte die Pumpe
-def startPump():
-        print("YEAAAH")
+import pyfirmata
+import time
 
+board = pyfirmata.Arduino('/dev/tty.usbmodem11301')
 
+while True:
+    board.digital[13].write(1)
+    time.sleep(10)
+    board.digital[13].write(0)
+    time.sleep(10)
